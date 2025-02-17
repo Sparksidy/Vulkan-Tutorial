77bb2dceaaf342886a26c58afb21b86b824878a8
Siddharth Gupta <sidystan@gmail.com>
2025-02-17 13:16:37 -0600

    Added window creation

--- Vulkan-Show/main.cpp
+++ Vulkan-Show/main.cpp
@@ -8,48 +8,50 @@
 
 #include <iostream>
 
+
+const uint32_t WIDTH = 800;
+const uint32_t HEIGHT = 600;
+
 class HelloTriangleApplication {
 
 public:
     void run() {
+        initWindow();
         initVulkan();
         mainLoop();
         cleanup();
     }
 
 private:
+    void initWindow() {
+        glfwInit();
+
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+        window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan Tutorial", nullptr, nullptr);
+    }
+
     void initVulkan() {
 
     }
 
     void mainLoop() {
-
+        while (!glfwWindowShouldClose(window)) {
+            glfwPollEvents();
+        }
     }
 
     void cleanup() {
-
+        glfwDestroyWindow(window);
+        glfwTerminate();
     }
+
+    GLFWwindow* window;
 
 };
 
 int main() {
-    /*glfwInit();
-
-    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-    GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan window", nullptr, nullptr);
-
-    uint32_t extensionCount = 0;
-    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
-
-    std::cout << extensionCount << " extensions supported\n";
-
-    while (!glfwWindowShouldClose(window)) {
-        glfwPollEvents();
-    }
-
-    glfwDestroyWindow(window);
-
-    glfwTerminate();*/
 
     HelloTriangleApplication app;
 
